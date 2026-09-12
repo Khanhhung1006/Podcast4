62179e2d3b154cee78660c0ee126279bf9e47fa0
Khanhhung1006 <136552632+Khanhhung1006@users.noreply.github.com>
2026-09-12 21:24:26 +0700

    Update AudioController.tsx

--- src/components/AudioController.tsx
+++ src/components/AudioController.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+// Chạy rỗng để tương thích ngược các giao diện khác không bị lỗi thiếu Import Component
 export default function AudioController() {
   return null;
 }
