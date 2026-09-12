0a1b3c406124120cc1c4b4b01f4782fae584d10a
Khanhhung1006 <136552632+Khanhhung1006@users.noreply.github.com>
2026-09-12 21:03:57 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,23 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { unlockAudioForMobile } from './store/playerStore';
-
-function initAudioUnlock() {
-  const unlock = () => {
-    try {
-      unlockAudioForMobile();
-    } catch (e) {
-      // Ignore
-    }
-  };
-
-  window.addEventListener('click', unlock, { passive: true, once: true });
-  window.addEventListener('touchstart', unlock, { passive: true, once: true });
-  window.addEventListener('touchend', unlock, { passive: true, once: true });
-}
-
-initAudioUnlock();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
